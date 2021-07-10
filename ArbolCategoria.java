45d2a4dca5358212d076978f807067dd79876937
Randy Aguero Bermudez <randyaguero1009@gmail.com>
2021-07-10 15:49:06 -0600

    Agregadas clases temporales, agregado metodo agregado de categorias

--- ArbolCategoria.java
+++ ArbolCategoria.java
@@ -4,16 +4,19 @@ public class ArbolCategoria
     int numeroLetra;
     ArbolCategoria letraIzquierda;//Letra izquierda para letras que necesitan previamente valor del nodo (letra);
     ArbolCategoria letraDerecha;//Letra derecha para letras que no necesita previamente el valor del nodo (letra);
-    ArbolCategoria()
+    ListaPel listaPeliculas;
+    ArbolCategoria(int num)
     {
-
+        numeroLetra = num;
+        letraIzquierda = null;
+        letraDerecha = null;
+        listaPeliculas = null;
     }
 
     public void busqueCategoria(String cat,boolean mostrarCoincidenciaExacta)
     {
         String categoria = cat;
         categoria = categoria.trim();
-        int tamanoCategoria = categoria.length();
         String letraAnalizar = categoria.substring(0,1);
         int numLetraAnalizar = deNumeroLetra(letraAnalizar);
 
@@ -35,9 +38,10 @@ public class ArbolCategoria
                 if(numLetraAnalizar==numeroLetra)//Ej: se ingresa ba(2)(1) y se esta en b(2); entonces si hay mas letras se va a la izquierda y empieza a buscar desde a(1)
                 //En otro caso se encontro una coincidencia y se mostrarian las peliculas iguales a categoria ba o con ba inicialmente
                 {
-                    if(tamanoCategoria>1)//Caso de existir mas letras a analizar
+                    categoria = categoria.substring(1);
+                    if(categoria.length()>1)//Caso de existir mas letras a analizar
                     {
-                        categoria = categoria.substring(1);//Se pasa al izquierda y se analiza usando la letra que sigue
+                        //Se pasa al izquierda y se analiza usando la letra que sigue
                         letraIzquierda.busqueCategoria(categoria, mostrarCoincidenciaExacta);
 
                     }
@@ -56,7 +60,7 @@ public class ArbolCategoria
                 else//Numero Letra menor que la del nodo //Ej, se busca b pero se esta en c, ya se sabe que no existe, de existir una coincidencia no llegaria a c., si no que se quedaria en b
                 //Ejemplo de rama (a)----(c) Si b>a (2>1), entonces a le dice a b que se vaya por el camino a la derecha, pero llega a c, entonces no existe un b guardado.
                 {
-                   
+
                     System.out.println("Error, no existe peliculas segun la categoria ingresada");
                 }
 
@@ -71,8 +75,89 @@ public class ArbolCategoria
 
     }
 
-    public void agregueCategoria(String cat)
+    public void agreguePelicula(String cat,Peli pelicula)//Una pelicula puede tener categorias distintas, entonces se debe ingresar una cat, y no se puede sacar de la pelicula
     {
+        agregueCategoriaNueva(cat);//No hace falta verificar si ya existe, por de hacerlo se tendria que recorrer el arbol dos veces, uno para ver si existe "x" categoria y la segunda para agregar letras que no existam
+        //El codigo va a ser eso en un solo proceso
+
+    }
+
+    public void agregueCategoriaNueva(String cat)
+    {
+        String categoria = cat;
+        categoria = categoria.trim();
+        int tamanoCategoria = categoria.length();
+        String letraAgregar = categoria.substring(0,1);
+        int numLetraAgregar = deNumeroLetra(letraAgregar);
+
+        if(numeroLetra != 0)//La idea para agregar una letra es similar a la busqueda.
+        {
+            if(numLetraAgregar>numeroLetra)//Ej: se ingresa b(2); y si se esta en a(1) entonces se verifica que exista alguien mayor que a y se manda b al siguiete
+            {
+                if(letraDerecha != null)
+                {
+                    letraDerecha.agregueCategoriaNueva(categoria);
+                }
+                else
+                {
+                    letraDerecha = new ArbolCategoria(numeroLetra);
+                    categoria = categoria.substring(1);
+                    if(categoria.length()>1)
+                    {
+                        letraDerecha.agregueCategoriaNueva(categoria);
+                    }
+                }
+            }
+            else
+            {
+                if(numLetraAgregar==numeroLetra)//Ej: se ingresa ba(2)(1) y se esta en b(2); entonces si hay mas letras se va a la izquierda y empieza a buscar desde a(1)
+                //En otro caso se encontro una coincidencia y se mostrarian las peliculas iguales a categoria ba o con ba inicialmente
+                {
+                    categoria = categoria.substring(1);
+                    if(categoria.length()>1)//Caso de existir mas letras a analizar
+                    {
+                        //Se pasa al izquierda y se analiza usando la letra que sigue
+                        if(letraIzquierda == null)
+                        {
+                            letraIzquierda = new ArbolCategoria(numeroLetra);
+                              letraIzquierda.agregueCategoriaNueva(categoria.substring(1));
+                        }
+                        else
+                        {
+                            letraIzquierda.agregueCategoriaNueva(categoria);
+                        }
+
+                    }
+                }
+
+            }
+        }
+        else
+        {
+            //"No existe la categoria"
+            numeroLetra = deNumeroLetra(letraAgregar);
+            if(categoria.length()>=1)
+            {
+                categoria = categoria.substring(1);
+            }
+            if(categoria.length()>=1)
+            {
+              
+                if(letraIzquierda == null)
+                {
+                    letraIzquierda = new ArbolCategoria(0);
+                    letraIzquierda.agregueCategoriaNueva(categoria.substring(1));
+                }
+                else
+                {
+                    letraIzquierda.agregueCategoriaNueva(categoria.substring(1));
+                }
+
+            }
+
+            //Por creacion del codigo no debera suceder esto, ya que de una u otra forma
+            //El usuario tendra ya subidas las categorias, 
+        }
 
     }
 
@@ -170,7 +255,7 @@ public class ArbolCategoria
 
     public static void main (String args[])
     {
-        ArbolCategoria arbol = new ArbolCategoria();
+        ArbolCategoria arbol = new ArbolCategoria(1);
         String cat = " E Gsdg " ;
         System.out.println("Categoria "+cat); 
         String categoria = cat.trim();
