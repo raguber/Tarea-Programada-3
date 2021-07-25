24fe82f3f53d97c26d3da2bf64a165263e7061a7
raguber <85461191+raguber@users.noreply.github.com>
2021-07-24 21:51:19 -0600

    Documentacion ArbolPeliculas

--- ArbolPeliculas.java
+++ ArbolPeliculas.java
@@ -1,8 +1,9 @@
 /**
  * Esta clase guarda peliculas ordenadas por hileras, donde las hileras pueden ser categorias, paises, entre otros,
  * Usa un valor asignado a caracteres de hileras alfabeticas para organizar el arbol
- * El funcionamiento del arbol basicamente se reduce a agregar a la derecha los valores mayores que un valor determinado, donde un valor derecho es un valor "poco importante"
- * Si se encuentra un valor igual se va a la izquierda, se corta la hilera, para ir guardando categorias por hileras.
+ * El funcionamiento del arbol basicamente se reduce a agregar a la derecha los valores mayores que un valor determinado, donde un valor derecho es un valor que se excluye cuando se compararan dos numero
+ * Si se encuentra un valor igual se va a la izquierda, se corta la hilera, para ir guardando categorias por numeros segun cada caracter.
+ * El arbol funciona con numeros, sin embargo, el guardado de lo "filtrado" se guarda tal y como se ingreso en una listaSimple, con objetos Pelicuña
  * 
  * @author Randy Agüero B90082, Frayvin Alvarado B60292 y Andrés Serrano C07483
  * @version 20/7/2021
@@ -15,6 +16,9 @@ public class ArbolPeliculas
     ArbolPeliculas letraDerecha;//Letra derecha para letras que no necesita previamente el valor del nodo (letra);
 
     ListaPeliculas listaPeliculas;
+    /**
+     * Constructor de ArbolPeliculas, deja valores nulos;
+     */
     ArbolPeliculas()
     {
         numeroLetra = 0;
@@ -23,6 +27,14 @@ public class ArbolPeliculas
 
         listaPeliculas = null;
     }
+
+    /**
+     * Este metodo es usado para mostrar un arbol por filtradado,
+     * Por ejemplo para mostrar categorias
+     * 
+     * @para arbol Este es el arbol a modificar
+     * @param mostrarPeliculas Este es usado para ver si se muestra cada pelicula;
+     */
 
     public void muestreFiltradoArbol(ArbolPeliculas arbol, boolean mostrarPeliculas)
     {
@@ -49,6 +61,16 @@ public class ArbolPeliculas
         }
 
     }
+
+    /**
+     * Este metodo agrega a un arbol una pelicula en una lista
+     * 
+     * @param arbol Este es el arbol que se va a modificar
+     * @param hil Hilera que se va a ir desintegrando para ordenar el arbol
+     * @param pelicula Esta es la pelicula que se agregaria
+     * @param hil Hilera que se va a guardar en la lista de peliculas tal y como se ingreso.
+     * 
+     */
 
     public void agreguePeliculaLista(ArbolPeliculas arbol,String hil,Pelicula pelicula,String hileraExacta)
     {
@@ -78,11 +100,8 @@ public class ArbolPeliculas
 
                         arbol.listaPeliculas = new ListaPeliculas(hileraExacta);
 
-                        
                     }
-                    
-                        arbol.listaPeliculas.agregue(pelicula);
-                    
+                    arbol.listaPeliculas.agregue(pelicula);
 
                 }
                 else//Se llego a una Hilera exacta
@@ -102,6 +121,15 @@ public class ArbolPeliculas
 
     }
 
+    /**
+     * Este metodo permite editar una categoria de un conjunto de peliculas
+     * 
+     * @param arbol Este es el arbol al que se le guardara la pelicula
+     * @param hilersExis Esta es la hilera que existe previamente guardada
+     *        se tomara una lista de peliculas con la palabra a filtrar. 
+     * @param nuevHilera Este es la nueva hilera que se le puede agregar a las peliculas (categorias);
+     */
+
     public void editeCategoria(ArbolPeliculas arbol,String hileraExis, String nuevHilera )
     {
         boolean existeCategoria = determineSiExisteHilera(arbol,hileraExis,false);
@@ -114,6 +142,15 @@ public class ArbolPeliculas
         editeCategoriaPeliculas(arbol,peliculas, nuevHilera);
 
     }
+
+    /**
+     * Este metodo dado va seleccionando pelicula por pelicula y la va a agregando a una nueva categoria
+     * 
+     * @param arbol Este es el arbol a que se le agregara las peliculas con nuevo filtrado
+     * @param peliculas Lista de peliculas a agregarles una nueva categoria
+     * @param nuevHil Nueva categoria para peliculas ya seleccionadas.
+     *
+     */
 
     public void editeCategoriaPeliculas(ArbolPeliculas arbol,ListaPeliculas peliculas,String nuevHil)
     {
@@ -137,12 +174,14 @@ public class ArbolPeliculas
         }
         arbol.determineSiExisteHilera(arbol,nuevHil, true);
     }
-    // }
 
-    public void muestreCategoria()
-    {
-
-    }
+    /**
+     *Este metodo devuelte una lista de peliculas dada una hilera
+     *
+     * @param arbol Este es el arbol al que se le solicita una lista de peliculas
+     * @param hilera Hilera que determinara cual lista se devolvera
+     * @return listaPeli Lista de peliculas que se encontro dada una hilera
+     */
 
     public ListaPeliculas retorneListaPeliculas(ArbolPeliculas arbol,String hilera)
     {
@@ -181,14 +220,23 @@ public class ArbolPeliculas
             }
 
         }
-      
+
         return listaPeli;
     }
 
-    public boolean determineSiExisteHilera(ArbolPeliculas arbol,String cat, boolean mostrarPeliculas)
+    /**
+     * Este metodo determina si dada una hilera existen peliculas agregadas
+     * @param arbol Este es el arbol al que se le buscara la existencia de peliculas
+     * @param hil Esta es la hilera que funciona como filtradado, esta se va descomponiendo
+     * @boolean mostrarPeliculas Este booleano permite mostrar si se le muestra o no las peliculas en caso de existencia dada una hilera
+     * @return existe Este valor sera true o false dependiendo de si existen peliculas con una categoria dada
+     * 
+     */
+
+    public boolean determineSiExisteHilera(ArbolPeliculas arbol,String hil, boolean mostrarPeliculas)
     {
         boolean existe = false;
-        String Hilera = cat;
+        String Hilera = hil;
 
         String letraAnalizar = Hilera.substring(0,1);
         int numLetraAnalizar = deNumeroLetra(letraAnalizar);
@@ -256,6 +304,13 @@ public class ArbolPeliculas
         return existe;
     }
 
+    /**
+     * Este metodo llamada a otro metodo para que vaya mandando diferentes hileras, dada la division previa de estas
+     * 
+     * @param arbol Arbol a agregarle hileras
+     * @param texto Texto a dividir para posteriormente enviar al arbol
+     */
+
     public void agregueHilera(ArbolPeliculas arbol,String texto)
     {
 
@@ -263,13 +318,12 @@ public class ArbolPeliculas
         String sub = "";
         String p = "";
         boolean siga = true;
-       
+
         while(siga){
             try{
 
                 pos=texto.indexOf(",");
-           
-              
+
 
                 if(pos != -1)
                 {
@@ -277,13 +331,13 @@ public class ArbolPeliculas
                     sub=texto.substring(0,pos);
 
                     texto=texto.substring(pos+1,texto.length());
-                    
+
                 }
                 else
                 {
-                    
+
                     sub=texto;
-                    
+
                     siga = false;
                 }
 
@@ -299,6 +353,16 @@ public class ArbolPeliculas
         }
     }
 
+    /**
+     * Este metodo funciona para agregar una pelicula dada una hilera definida
+     * 
+     * @param arbol Arbol a ser agregada una pelicula
+     * @param texto a ser usado para dividir el texto en pequeños trozos y agregar peliculas en los lugares correspondientes
+     * @param pelicula pelicula a ser guardada en un lugar del arbol dada una hilera
+     * @return arbolAGuardad devuelve el arbol con la pelicula guardad
+     * 
+     */
+
     public ArbolPeliculas agreguePelicula(ArbolPeliculas arbol, String texto,Pelicula pelicula)//Una pelicula puede tener hilera distintas, entonces se debe ingresar una cat, y no se puede sacar de la pelicula
     {
 
@@ -307,7 +371,7 @@ public class ArbolPeliculas
         String sub = "";
         String p = "";
         boolean siga = true;
-        
+
         while(siga){
             try{
 
@@ -322,11 +386,11 @@ public class ArbolPeliculas
                 }
                 else
                 {
-                    
+
                     sub=texto;
-                    
+
                     sub=texto;
-                    
+
                     siga = false;
                 }
 
@@ -340,9 +404,18 @@ public class ArbolPeliculas
 
     }
 
-    public void agregueHileraNueva(ArbolPeliculas arbol,String cat)
+    /**
+     * Este metodo agrega una hilera en el arbol, la hilera se va descomponiendo por letras (guardadas como numeros)
+     * el metodo determina si un numero es igual a otro , en caso de ser igual se agrega a la izquierda y se va desintegrando la hilera
+     * En caso de ser mayo una letra a agregar y una letra se envia la hilera a la derecha, y se continua el mismo procedimiento
+     * 
+     * @param arbol Este arbol es al que se le va a agregar una hilera
+     * @param cat Esta es la hilera a agregar
+     */
+
+    public void agregueHileraNueva(ArbolPeliculas arbol,String hil)
     {
-        String Hilera = cat;
+        String Hilera = hil;
 
         int tamanoHilera = Hilera.length();
         String letraAgregar = Hilera.substring(0,1);
@@ -492,6 +565,11 @@ public class ArbolPeliculas
         }
 
     }
+
+    /**
+     * Este metodo devuelve un valor numero determinado, de acuerdo a un caracter
+     * Se envia un numero del 1 al 27 si es una letra, 28 si es un espacio y 30 en cualquier otro caso
+     */
 
     public int deNumeroLetra(String L)
     {
